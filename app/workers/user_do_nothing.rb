090fedf4554348c6257e8da71d8892a343995191
Damjan Znidarsic <damjan.znidarsic@dlabs.si>
2017-03-23 21:54:17 +0000

    changes

--- app/workers/user_do_nothing.rb
+++ app/workers/user_do_nothing.rb
@@ -2,13 +2,13 @@ class UserDoNothing
     include Sidekiq::Worker
 
     def perform(*arguments)
-        cmd = "php bin/console worker:exec --decode"
+        cmd = "php bin/console worker:exec --decode -e #{ENV['RAILS_ENV']} --no-debug"
         args = Base64.encode64(gzip(arguments.to_json))
 
         logger.info "Calling #{cmd} dlabs.user.queue_handler.do_nothing '#{args}'"
 
         # call SF2 command and redirect stderr with 2>&1
-        output=`#{cmd} dlabs.user.queue_handler.do_nothing --no-debug '#{args}' 2>&1`; result=$?.success?
+        output=`#{cmd} dlabs.user.queue_handler.do_nothing '#{args}' 2>&1`; result=$?.success?
 
         if result != true
             raise output
